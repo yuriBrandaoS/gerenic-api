2c5e23e678bfb6a568db23f00462e51ec675f1be
Yuri <yuri.soares@klickpages.com.br>
2020-03-04 13:37:57 -0300

    Adiciona estrutura de model

--- src/app.js
+++ src/app.js
@@ -1,6 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Mentions = require('./models/mentions');
+
+require('dotenv').config();
 
 const app = express();
+
+//Database
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: true,
+	useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+	console.log('Mongoose connection is open');
+});
+
+db.error('error', err => {
+	console.log(`Mongoose default connection has occured \n${err}`);
+});
+
+db.on('disconnected', () => {
+	console.log('Mongoose default connection is disconnected');
+	
+});
+
+process.on('SIGINT', () => {
+	db.close(() => {
+		console.log('Mongoose default connection is disconnected due to application termination');
+	});
+
+	process.exit(0);
+});
 
 const indexRoutes = require('./routes/index-routes');
 app.use('/', indexRoutes);

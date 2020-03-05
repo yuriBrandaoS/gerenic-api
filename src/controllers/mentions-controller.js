fd9b8fe8ca4879e4bda50383cdbe0488d0cf1433
Yuri <yuri.soares@klickpages.com.br>
2020-03-05 11:13:15 -0300

    adiciona método de update nas mentions

--- src/controllers/mentions-controller.js
+++ src/controllers/mentions-controller.js
@@ -32,3 +32,21 @@ exports.createMention = async(req, res) => {
 		res.status(500).send({message: 'fail register'});
 	}
 };
+
+//update
+exports.updateMention = async(req, res) => {
+	const { errors }= validationResult(req);
+
+	if(errors.length > 0){
+		return res.status(400).send({ message: errors });
+	}
+
+	try {
+		await repository.updateMention(req.params.id, req.body);
+		res.status(200).send({
+			message: 'Mention updated with success!'
+		});
+	} catch(e) {
+		res.status(500).send({ message: 'Fail update' });
+	}
+};
